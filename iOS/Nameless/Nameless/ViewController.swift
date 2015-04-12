36bfc6dff90eef9da63fe659991f7d4f86861596
Ethan Thomas <ethanthomas33@gmail.com>
2015-04-11 20:36:22 -0400

    Added Parse Frameworks for iOS

--- iOS/Nameless/Nameless/ViewController.swift
+++ iOS/Nameless/Nameless/ViewController.swift
@@ -9,10 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let context = CIContext(options: nil)
+    
     override func viewDidLoad() {
+        var connectMeButton: UIButton = UIButton(frame: CGRectMake(self.view.frame.width / 2, self.view.frame.height / 2, 290, 90));
+        connectMeButton.setTitle("Connect Me", forState: nil);
+        connectMeButton.tintColor = UIColor.greenColor()
+        connectMeButton.setTitleColor(UIColor.greenColor(), forState: nil)
+        self.view.addSubview(connectMeButton);
+        
+//        let testFrame : CGRect = CGRectMake(0,200,320,200)
+//        var testView : UIView = UIView(frame: testFrame)
+//        testView.backgroundColor = UIColor(red: 0.5, green: 0.5, blue: 0.5, alpha: 1.0)
+//        testView.alpha=0.5
+//        self.view.addSubview(testView)
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
